dc6da0d9e8259e939342726044ab6b55c3b3a3cd
Amunra <hungnguyentrong285@gmail.com>
2024-05-25 19:27:13 +0700

    Tạo ra giao diện register và viết backend cho register sử dụng cors, jwt, cookie-parser, prisma, axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import HomePage from "./routes/homePage/homePage"
+import HomePage from "./routes/homePage/homePage";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -7,35 +7,45 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
 
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout/>,
-        children:[
-          {
-            path: "/",
-            element: <HomePage/>
-          },
-          {
-            path: "/list",
-            element: <ListPage/>
-          },
-          {
-            path: "/:id",
-            element: <SinglePage/>
-          },
-          {
-            path: "/profile",
-            element: <ProfilePage/>
-          }
-        ]
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        {
+          path:"/profile",
+          element:<ProfilePage/>
+        },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
+        }
+      ]
     }
   ]);
 
   return (
+
     <RouterProvider router={router}/>
   );
 }
